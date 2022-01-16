a3d5300269f68867828cf668929686cc9674eeab
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-16 23:13:29 +0530

    Update replace.js

--- js/replace.js
+++ js/replace.js
@@ -3,7 +3,7 @@ var fs = require("fs")
 
 glob("scss/*.scss", undefined, function (er, files) {
   files.forEach(file => {
-    fs.writeFileSync(file, fs.readFileSync(file).toString().replace(/[\w-]+:( \$[\w-]+)+;/g, _ => {
+    fs.writeFileSync(file, fs.readFileSync(file).toString().replace(/ [\w-]+:( \$[\w-]+)+;/g, _ => {
       return _.replace(/ \$([\w-]+)/g, (__, n) => ` var(--#{$variable-prefix}${n}, $${n})`)
     }));
   })
