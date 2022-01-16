787916d2115655bc62893827e6380d92a048c084
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2022-01-16 22:34:18 +0530

    Create replace.js

---
+++ js/replace.js
@@ -0,0 +1,10 @@
+var glob = require("glob")
+var fs = require("fs")
+
+glob("scss/*.scss", undefined, function (er, files) {
+  files.forEach(file => {
+    fs.writeFileSync(file, fs.readFileSync(file).toString.replace(/[\w-]+:( \$[\w-]+)+;/g, _ => {
+      return _.replace(/ \$([\w-]+)/g, (__, n) => `var(--#{$variable-prefix}${n}, $${n})`)
+    }));
+  })
+})
